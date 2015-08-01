115be7c460d409bbcc104b76a78811cf1d8a1e61
dkoo <derrick_koo@newyorker.com>
2015-08-01 18:00:34 -0400

    all the fields

--- client/js/yumhut.js
+++ client/js/yumhut.js
@@ -1,8 +1,17 @@
-Yums = new Mongo.Collection('yums');
+var yumhut = (function() {
+	
+	// init Yums collection
+	Yums = new Mongo.Collection('yums');
 
-if (Meteor.isClient) {
+	// watch the yums template for changes
 	Meteor.subscribe('yums');
 
+	// startup actions
+	Meteor.startup(function() {
+		Session.set('adding', false);
+	});
+
+	// body helpers
 	Template.body.helpers({
 		yums: function() {
 			if ( Session.get('filtered') ) {
@@ -10,29 +19,93 @@ if (Meteor.isClient) {
 			} else {
 				return Yums.find({}, {sort: {createdAt: -1}});
 			}
+		}
+	});
+
+	// add yum helpers
+	Template.add.helpers({
+		adding: function() {
+			return Session.get('adding');
 		},
 		filtered: function() {
 			return Session.get('filtered');
 		}
 	});
 
-	Template.body.events({
+	// add yum events
+	Template.add.events({
 		'submit .new-yum': function(e) {
 			e.preventDefault();
 
-			var input = e.target.name.value;
+			// enter add mode
+			Session.set('adding', true);
 
-			// add a Yum
-			Meteor.call('addYum', input);
+			// retrieve autocomplete results
+			var place = Session.get('place'),
+				placeDetails = {};
+
+			// if there was a Google place result
+			if ( place ) {
+				placeDetails.id = place.place_id;
+				placeDetails.loc = place.geometry.location;
+			} else {
+				// otherwise, just give it a random unique id
+				placeDetails.id = randomString(6);
+			}
+
+			// get the inputted values
+			placeDetails.name = e.target.add_name.value;
+			placeDetails.address = e.target.add_address.value;
+			placeDetails.favs = e.target.add_favs.value;
+			placeDetails.notes = e.target.add_notes.value;
+
+			// add the Yum{
+			Meteor.call('addYum', placeDetails);
 
 			// clear after submitting
 			e.target.name.value = '';
+
+			// delete Session info after submitting
+			Session.set('place', null);
 		},
 		'change .filter input': function(e) {
 			Session.set('filtered', e.target.checked);
+		},
+		'click button.add': function(e) {
+			e.preventDefault();
+
+			// enter "add" mode
+			Session.set('adding', true);
+			var searchBar = document.getElementById('autocomplete'),
+				address = document.getElementsByName('add_address')[0],
+				place,
+				autocomplete = new google.maps.places.Autocomplete(
+					searchBar, {types: ['establishment'] }
+				);
+
+			// set up the Google Places search bar
+			google.maps.event.addListener(autocomplete, 'place_changed', function() {
+				setPlace(searchBar, autocomplete.getPlace());
+
+				place = Session.get('place');
+
+				if ( place ) {
+					searchBar.value = place.name;
+					address.value = place.formatted_address;
+				}
+			});
+		},
+		'click .new-yum button.cancel': function(e) {
+			e.preventDefault();
+
+			var el = document.getElementById('autocomplete');
+			el.value = '';
+
+			Session.set('adding', false);
 		}
 	});
 
+	// count helpers
 	Template.count.helpers({
 		showCount: function() {
 			return Yums.find({checked: {$ne: true}}).count();
@@ -42,44 +115,124 @@ if (Meteor.isClient) {
 		}
 	});
 
+	// when yums first load, exit edit mode
+	Template.yum.onRendered(function() {
+		Meteor.call('editYum', this.data._id, 'cancel');
+	});
+
+	// yum helpers
 	Template.yum.helpers({
 		isOwner: function() {
 			return this.owner === Meteor.userId();
+		},
+		listFavs: function() {
+			return this.favs.join('\n');
 		}
 	});
 
+	// yum events
 	Template.yum.events({
+		// delete a yum
 		'click .delete': function() {
 			Meteor.call('deleteYum', this._id, this.username );
 		},
-		'click .edit': function() {
+		// enter "edit" mode
+		'click .edit': function(e) {
 			Meteor.call('editYum', this._id, 'edit');
+			var parent = e.currentTarget.parentNode.parentNode,
+				id = this.id,
+				yumname = document.getElementsByName('editname_' + id)[0],
+				address = document.getElementsByName('editaddress_' + id)[0],
+				autocomplete = new google.maps.places.Autocomplete(
+					yumname, {types: ['establishment'] }
+				);
+
+			google.maps.event.addListener(autocomplete, 'place_changed', function() {
+				setPlace(yumname, autocomplete.getPlace());
+
+				place = Session.get('place');
+
+				if ( place ) {
+					yumname.value = place.name;
+					address.value = place.formatted_address;
+				}
+			});
 		},
-		'click .cancel': function() {
+		// cancel "edit" mode
+		'click .cancel': function(e) {
+			e.preventDefault();
+			var parent = e.currentTarget.parentNode.parentNode,
+				yumname = document.getElementsByName('editname_' + this.id)[0],
+				address = document.getElementsByName('editaddress_' + this.id)[0];
+
+			yumname.value = '';
+			address.value = '';
+			Session.set('place', null);
 			Meteor.call('editYum', this._id, 'cancel');
 		},
+		// edit the yum
 		'submit .edit-yum': function(e) {
 			e.preventDefault();
 
-			var input = e.target.name.value;
+			// retrieve autocomplete results
+			var place = Session.get('place'),
+				yumname = document.getElementsByName('editname_' + this.id)[0],
+				address = document.getElementsByName('editaddress_' + this.id)[0],
+				// favs = document.getElementsByName('editfavs_' + this.id)[0],
+				notes = document.getElementsByName('editnotes_' + this.id)[0],
+				placeDetails = {};
 
-			// edit Yum
-			Meteor.call('updateYum', this._id, input);
+			// if there's a Google result, reset the id and geolocation
+			if ( place ) {
+				placeDetails.id = place.place_id;
+				placeDetails.loc = place.geometry.location;
+			} else {
+				// otherwise, keep the same id and loc
+				placeDetails.id = this.id;
+				placeDetails.loc = this.loc;
+			}
 
-			// reset form
-			e.target.name.value = input;
+			placeDetails.name = yumname.value;
+			placeDetails.address = address.value ? address.value : this.address;
+			// placeDetails.favs = favs.value ? favs.value : this.favs;
+			placeDetails.notes = notes.value ? notes.value : this.notes;
+
+			// update the Yum{
+			Meteor.call('updateYum', this._id, placeDetails, this.username);
+
+			// clear form after submitting
+			yumname.value = '';
+			address.value = '';
+			// favs.value = '';
+			notes.value = '';
+
+			// clear Session info after submitting
+			Session.set('place', null);
 		}
 	});
 
+	// config for accounts.ui package
 	Accounts.ui.config({
 		passwordSignupFields: "USERNAME_ONLY"
 	});
-}
 
-if (Meteor.isServer) {
-	Meteor.publish('yums', function () {
-		return Yums.find({
-			owner: this.userId
-		});
-	});
-}
+	// temporarily store place info in Session
+	setPlace = function(el, place) {
+		if ( place.place_id ) {
+			var placeId = place.place_id;
+
+			Session.set('place', place);
+		} else {
+			return;
+		}
+	};
+
+	function randomString(length) {
+		var text = "";
+		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		for(var i = 0; i < length; i++) {
+				text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+		return text;
+	}
+}());
--- server/js/db.js
+++ server/js/db.js
@@ -1,12 +1,27 @@
+// init Yums collection
+Yums = new Mongo.Collection('yums');
+
+Meteor.publish('yums', function () {
+	return Yums.find({
+		owner: this.userId
+	});
+});
+
+
+// db methods
 Meteor.methods({
-	addYum: function(input) {
+	addYum: function(place) {
 		// user must be logged in to add yums
 		if ( !Meteor.userId() ) {
 			throw new Meteor.Error('not-authorized');
 		}
-
 		Yums.insert({
-			name: input,
+			name: place.name,
+			id: place.id,
+			loc: place.loc,
+			address: place.address,
+			favs: place.favs.split('\n'),
+			notes: place.notes,
 			createdAt: new Date(),
 			owner: Meteor.userId(),
 			username: Meteor.user().username
@@ -28,12 +43,27 @@ Meteor.methods({
 			}
 		});
 	},
-	updateYum: function(yumId, input) {
-		Yums.update(yumId, {
-			$set: {
-				name: input,
-				editing: false
+	updateYum: function(yumId, place, username) {
+		if ( Meteor.user().username === username ) {
+			if ( place ) {
+				Yums.update(yumId, {
+					$set: {
+						name: place.name,
+						id: place.id,
+						loc: place.loc,
+						address: place.address,
+						notes: place.notes,
+						editing: false
+					}
+				});
+			} else {
+				Yums.update(yumId, {
+					$set: {
+						editing: false
+					}
+				});
+				return false;
 			}
-		});
+		}
 	}
 });
