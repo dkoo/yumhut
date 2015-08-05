efa7dd888c3c3677412e13922c440dcb11b9fa43
dkoo <derrick_koo@newyorker.com>
2015-08-04 22:44:22 -0400

    add autoprefixer and tweak paths

--- private/gulpfile.js
+++ private/gulpfile.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
 
 /* lint js files */
 gulp.task('lint', function() {
@@ -15,12 +16,23 @@ gulp.task('lint', function() {
 gulp.task('sass', function() {
 	return gulp.src('../client/stylesheets/styles.scss')
 		.pipe(sass({outputStyle: 'compressed'}))
-		.pipe(gulp.dest('../client/stylesheets/'));
+		.pipe(gulp.dest('assets/stylesheets/unprefixed'));
+});
+
+/* post-process CSS with autoprefixer */
+gulp.task('autoprefix', function() {
+	return gulp.src('assets/stylesheets/unprefixed/styles.css')
+		.pipe(autoprefixer({
+			browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('../client/stylesheets/compiled'));
 });
 
 /* watch files for changes and execute tasks */
 gulp.task('watch', function() {
 	gulp.watch('../**/*.scss', ['sass']);
+	gulp.watch('*.css', ['autoprefix']);
 	gulp.watch(['../client/**/*.js', '../server/**/*.js'], ['lint']);
 });
 
