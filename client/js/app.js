646525006c30a101869caf77b9a0279d44694691
dkoo <derrick_koo@newyorker.com>
2015-08-29 23:29:25 -0400

    login flow

--- client/js/app.js
+++ client/js/app.js
@@ -10,22 +10,9 @@ Meteor.startup(function() {
 	Session.set('deleting', false);
 });
 
-// config for accounts.ui package
-Accounts.ui.config({
-	passwordSignupFields: "USERNAME_ONLY"
-});
-
 // body helpers
 Template.body.helpers({
 	loading: function() {
 		return Session.get('loading');
-	}
-});
-
-
-// reset default class names when logging out
-Template.loginButtons.events({
-	'click #login-buttons-logout': function() {
-		document.body.classList.remove('user');
 	}
 });
--- client/js/modules/accounts/login.js
+++ client/js/modules/accounts/login.js
@@ -6,5 +6,59 @@ Template.login.events({
 	},
 	'submit #login': function(e) {
 		e.preventDefault();
-	}
+
+		if ( Session.get('created') ) {
+			Session.set('created', false);
+		}
+
+		var user = {
+				name: e.target.user.value.trim().toLowerCase(),
+				password: e.target.password.value,
+				errors: document.getElementById('errors')
+			},
+			// possible error messages
+			messages = {
+				email: '<p><i class="fa fa-exclamation-circle"></i> Please enter a valid e-mail address.</p>',
+				password: '<p><i class="fa fa-exclamation-circle"></i> Please enter a login password.</p>',
+				userNotFound: '. Do you need to <a href="#" class="register">register</a>?',
+				wrongPassword: '. Try entering your password again.'
+			};
+
+		// validate email and password
+		if ( !user.name || !user.password ) { // if there's no email/password or the email doesn't contain an @ character
+			if ( !user.name ) {
+				errors.innerHTML = messages.username;
+				if ( !user.password ) {
+					errors.innerHTML += messages.password;
+				}
+			} else {
+				if ( !user.password ) {
+					errors.innerHTML = messages.password;
+				}
+			}
+			return false;
+		} else {
+			Session.set('loading', true);
+			// if inputs pass validation, attempt to log in with the given email and password
+			Meteor.loginWithPassword(user.name, user.password, function(err){
+				// handle callback errors
+				if (err) {
+					console.log(err);
+					var reason = err.reason;
+					if ( reason === 'User not found' ) {
+						errors.innerHTML = reason + messages.userNotFound;
+					} else if ( reason === 'Incorrect password' ) {
+						errors.innerHTML = reason + messages.wrongPassword;
+					}
+				} else {
+					// successful login
+					console.log('logging in');
+					Session.set('menu', false);
+				}
+				Session.set('loading', false);
+			});
+			return false;
+		}
+	},
+
 });
